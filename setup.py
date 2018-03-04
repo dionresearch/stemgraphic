1774ee63b3a1ea7b0d68fd3ac1ce639acbe341f8
fdion <francois.dion@gmail.com>
2018-03-04 09:47:47 -0500

    changed from standard blue to categorical C0

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import io
+import os
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+with io.open(os.path.join(here, 'VERSION')) as f:
+    version = f.read()
+
 setup(name='stemgraphic',
-      version='0.5.2',
+      version=version,
       install_requires=[
           "docopt",
           "matplotlib",
