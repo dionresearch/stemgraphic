f5a0706e3389beb3b477ef4c039b65d8afef3fec
fdion <francois.dion@gmail.com>
2019-03-26 16:20:32 -0400

    documentation

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -134,22 +134,21 @@ htmlhelp_basename = 'stemgraphicdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+    'preamble': '''
+% Enable unicode and use Courier New
+\usepackage{fontspec}
+\setmonofont{Courier New}
+''',
+ # disable font inclusion
+ 'fontpkg': '',
+ 'fontenc': '',
+ # Fix Unicode handling by disabling the defaults for a few items
+ # set by sphinx
+ 'inputenc': '',
+ 'utf8extra': '',
 }
+
+latex_engine = "xelatex"
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
