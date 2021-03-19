81761dbaf042de3129ebbc770fd49a8c95029d79
Francois Dion <fdion@dionresearch.com>
2021-03-19 16:15:44 -0400

    version from module itself

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ import io
 import os
 from setuptools import setup
 
+from stemgraphic import __version__ as version
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
-
-with io.open(os.path.join(here, 'VERSION')) as f:
-    version = f.read()
 
 setup(name='stemgraphic',
       version=version,
